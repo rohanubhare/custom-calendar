d28b33e7b457c5512136d854158684700acccfdc
rohanubhare <rohan.ubhare@gmail.com>
2022-12-21 23:02:09 +0530

    Folder structuring

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Custom Calendar Home Page'),
+      home: HomePage(),
     );
   }
 }
